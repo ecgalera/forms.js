be751fb4cf06ce12b0b2010b52b6e4e33e1a9ae2
Eduardo Galera <galera.eduardo@gmail.com>
2021-09-29 09:20:21 -0300

    Trabajo Terminado

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ function validarFormulario(){
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
     let correo = document.getElementById("correo").value;
-    let fecha = docuement.getElementById("fecha").value;
+    let fecha = document.getElementById("fecha").value;
     let selector = document.getElementById("selector").selectedIndex;
     let checkbox = document.getElementById("checkBox").value;
     let radiobtn = document.getElementById("radioButton").value;
     let boleana = false;
   
-    // test obligatorio 
+    
     if(!nombre || nombre.length == 0){
         alert("Error: El campo nombre no debe ir vacio o con espacios en blanco");
         return false;
     }
-    // test edad
+    
 
     if(!edad || edad.length == 0 || isNaN(edad)){
         alert("Error: Debe ingresar una edad");
         return false;
     }
 
-    //test correo
+   
     if((!(/\S+@\S+\.\S+/.test(correo)))){
         alert("Error: Escribir un correo válido");
         return false;
